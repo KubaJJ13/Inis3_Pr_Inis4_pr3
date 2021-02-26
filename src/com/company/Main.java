ea7bc03c111a092e1c08f8d8c5626f41420aab70
KubaJJ13 <jazkuba19@wp.pl>
2021-02-26 17:18:04 +0100

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here\
+
+       Animal dog = new Animal();
+       dog.species="dog";
+       dog.name="haiku";
+       dog.weight=8.0;
+       dog.feed();
+
+       Phone iPhone = new Phone();
+       iPhone.screenSize=4.5;
+       iPhone.producer="apple";
+       iPhone.model="6S";
+       iPhone.operationSystem="ios";
+    }
+}
