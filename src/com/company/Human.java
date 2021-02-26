969fb04e3dfd48dcad4b4ece762877e5bcc92822
KubaJJ13 <jazkuba19@wp.pl>
2021-02-26 17:56:51 +0100

    initial commit 3

---
+++ src/com/company/Human.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Human {
+    String FirstName;
+    String LastName;
+    String phone;
+    Animal pet;
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here\
+       // write your code here\
 
        Animal dog = new Animal();
-       dog.species="dog";
-       dog.name="haiku";
-       dog.weight=8.0;
+       dog.species = "dog";
+       dog.name = "haiku";
+       dog.weight = 8.0;
        dog.feed();
 
        Phone iPhone = new Phone();
-       iPhone.screenSize=4.5;
-       iPhone.producer="apple";
-       iPhone.model="6S";
-       iPhone.operationSystem="ios";
+       iPhone.screenSize = 4.5;
+       iPhone.producer = "apple";
+       iPhone.model = "6S";
+       iPhone.operationSystem = "ios";
+
+       Human me = new Human();
+       me.FirstName = "Artur";
+       me.LastName = "JJ";
+       me.pet = dog;
+
+       System.out.println("Zwierzak nazywa się: " + me.pet.name);
     }
 }
